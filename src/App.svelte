7bef37a56a51f45956a122b5de9a878478a7bc36
Th1nkK1D <witheep@gmail.com>
2021-03-29 16:04:52 +0700

    Remove unused assets

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,15 @@
-<script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
-</script>
+<main class="text-center h-screen flex">
+  <div class="m-auto space-y-2">
+    <h1 class="text-orange-500 text-5xl">Hello Svelte Typescript!</h1>
 
-<main class="text-center">
-  <img src={logo} alt="Svelte Logo" class="m-auto" />
-  <h1 class="text-orange-500 text-5xl">Hello Typescript!</h1>
+    <p>
+      Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+      apps.
+    </p>
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+    <p>
+      Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+      the officially supported framework, also powered by Vite!
+    </p>
+  </div>
 </main>
