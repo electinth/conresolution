323f54626c37800c4fba10c83ea38506a4bee4fe
Th1nkK1D <witheep@gmail.com>
2021-03-29 16:02:23 +0700

    Add windi css

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,9 @@
   import Counter from './lib/Counter.svelte'
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
+<main class="text-center">
+  <img src={logo} alt="Svelte Logo" class="m-auto" />
+  <h1 class="text-orange-500 text-5xl">Hello Typescript!</h1>
 
   <Counter />
 
@@ -19,47 +19,3 @@
     the officially supported framework, also powered by Vite!
   </p>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
