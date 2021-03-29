973123eb1b8766f38fbff905dcd8f250ca17219a
Th1nkK1D <witheep@gmail.com>
2021-03-29 18:43:58 +0700

    Add airtable count fetch utility

---
+++ src/utils/fetch.js
@@ -0,0 +1,49 @@
+const Airtable = require('airtable')
+const { writeFileSync } = require('fs')
+
+require('dotenv').config()
+
+const FIELD_COUNT = 'Count'
+const FIELD_DATE = 'Date'
+const OUTPUT_DIR = './src/count.json'
+
+const table = new Airtable({ apiKey: process.env.AIRTABLE_KEY }).base(
+  process.env.AIRTABLE_BASE
+)(process.env.AIRTABLE_TABLE)
+
+const fetchRecords = () =>
+  new Promise((resolve, reject) => {
+    let records = []
+
+    table
+      .select({
+        view: 'Grid view',
+      })
+      .eachPage(
+        (pageRecords, fetchNextPage) => {
+          records = [...records, ...pageRecords]
+
+          fetchNextPage()
+        },
+        (err) => {
+          if (err) reject(err)
+
+          resolve(records)
+        }
+      )
+  })
+
+fetchRecords()
+  .then((records) => {
+    const count = records.reduce(
+      (sum, record) => sum + record.get(FIELD_COUNT),
+      0
+    )
+    const lastUpdated = records[records.length - 1].get(FIELD_DATE)
+
+    writeFileSync(OUTPUT_DIR, JSON.stringify({ count, lastUpdated }))
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
