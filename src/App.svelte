689df35438831ec38c85d6fa2cccedd727e84d94
Th1nkK1D <witheep@gmail.com>
2021-03-29 19:07:44 +0700

    Display count data on main page

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,16 @@
+<script lang="ts">
+  import countData from './count.json'
+
+  const lastUpdated = new Date(countData.lastUpdated).toLocaleDateString(
+    'th-TH'
+  )
+</script>
+
 <main class="text-center h-screen flex">
   <div class="m-auto space-y-2">
-    <h1 class="text-orange-500 text-5xl">Hello Svelte Typescript!</h1>
+    <h1 class="text-blue-500 text-5xl">CONRESOLUTION</h1>
 
-    <p>
-      Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-      apps.
-    </p>
-
-    <p>
-      Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-      the officially supported framework, also powered by Vite!
-    </p>
+    <p>Total: {countData.count}</p>
+    <p>Last updated: {lastUpdated}</p>
   </div>
 </main>
