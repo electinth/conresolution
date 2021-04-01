60284378320b803f0ab98a84875aa3e7871bcbd9
Th1nkK1D <witheep@gmail.com>
2021-04-01 23:48:08 +0700

    Styling scroll navigation button

--- src/components/how/horizontal-scroll.svelte
+++ src/components/how/horizontal-scroll.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte'
   import Swiper from 'swiper'
   import 'swiper/swiper-bundle.css'
+  import NavigationButton from './navigation-button.svelte'
 
   let container: HTMLElement
   let swiper: Swiper
@@ -31,14 +32,62 @@
   })
 </script>
 
-<div bind:this={container} class="swiper-container -m-8">
+<div bind:this={container} class="swiper-container -m-4 md:-m-8 relative">
   <div class="swiper-wrapper">
     <slot />
   </div>
   {#if showPrevButton}
-    <div class="swiper-button-prev" on:click={() => swiper.slidePrev()} />
+    <NavigationButton direction="prev" on:click={() => swiper.slidePrev()}>
+      <svg
+        width="19"
+        height="11"
+        viewBox="0 0 19 11"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M7.25 1.125L2 5.5L7.25 9.875"
+          stroke="#0E3431"
+          stroke-width="2"
+        />
+        <line
+          x1="2"
+          y1="5.125"
+          x2="18.25"
+          y2="5.125"
+          stroke="#0E3431"
+          stroke-width="2"
+        />
+      </svg>
+    </NavigationButton>
   {/if}
   {#if showNextButton}
-    <div class="swiper-button-next" on:click={() => swiper.slideNext()} />
+    <NavigationButton direction="next" on:click={() => swiper.slideNext()}>
+      <p class="text-b3 text-green-1 font-bold">
+        ประเทศ<br />จะดีขึ้น<br />อย่างไร
+      </p>
+      <svg
+        width="19"
+        height="12"
+        viewBox="0 0 19 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="mx-auto"
+      >
+        <path
+          d="M11.75 10.375L17 6L11.75 1.625"
+          stroke="#0E3431"
+          stroke-width="2"
+        />
+        <line
+          x1="17"
+          y1="6.375"
+          x2="0.75"
+          y2="6.375"
+          stroke="#0E3431"
+          stroke-width="2"
+        />
+      </svg>
+    </NavigationButton>
   {/if}
 </div>
---
+++ src/components/how/navigation-button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let direction: 'prev' | 'next'
+
+  const divClassMap = {
+    prev: 'left-0',
+    next: 'right-0',
+  }
+
+  const buttonClassMap = {
+    prev: 'rounded-r-lg',
+    next: 'rounded-l-lg',
+  }
+</script>
+
+<div class="absolute top-0 bottom-0 {divClassMap[direction]} flex z-10">
+  <button
+    class="bg-white {buttonClassMap[
+      direction
+    ]} my-auto text-center h-20 px-3 space-y-2 opacity-60"
+    on:click
+  >
+    <slot />
+  </button>
+</div>
