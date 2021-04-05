0094e2fa0bda60372b6c7f63412dacd62ac9691d
Th1nkK1D <witheep@gmail.com>
2021-04-05 13:41:37 +0700

    Remove link from footer logo

--- src/sections/footer.svelte
+++ src/sections/footer.svelte
@@ -2,13 +2,13 @@
   import FollowUs from '../components/follow-us.svelte'
   import Layout from '../components/layout.svelte'
 
-  const links = [
-    { alt: 'Resolution', src: 'images/logo-resolution.png', href: '#' },
-    { alt: 'iLaw', src: 'images/logo-ilaw.png', href: '#' },
-    { alt: 'CONLAB', src: 'images/logo-conlab.png', href: '#' },
-    { alt: 'คณะก้าวหน้า', src: 'images/logo-klaona.png', href: '#' },
-    { alt: 'พรรคก้าวไหล', src: 'images/logo-klaokrai.png', href: '#' },
-    { alt: 'ELECT', src: 'images/logo-elect.png', href: '#' },
+  const logos = [
+    { alt: 'Resolution', src: 'images/logo-resolution.png' },
+    { alt: 'iLaw', src: 'images/logo-ilaw.png' },
+    { alt: 'CONLAB', src: 'images/logo-conlab.png' },
+    { alt: 'คณะก้าวหน้า', src: 'images/logo-klaona.png' },
+    { alt: 'พรรคก้าวไหล', src: 'images/logo-klaokrai.png' },
+    { alt: 'ELECT', src: 'images/logo-elect.png' },
   ]
 </script>
 
@@ -20,15 +20,8 @@
   <div
     class="flex justify-between py-6 md:py-12 w-full max-w-screen-xl mx-auto"
   >
-    {#each links as { alt, src, href }}
-      <a
-        {href}
-        target="_blank"
-        rel="noopener noreferrer"
-        class="my-auto max-w-16 md:max-w-24"
-      >
-        <img {src} {alt} />
-      </a>
+    {#each logos as { alt, src }}
+      <img {src} {alt} class="my-auto max-w-16 md:max-w-24" />
     {/each}
   </div>
 </Layout>
