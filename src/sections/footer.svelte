0f8b62d258b0f36030dea6bc00493c89bc083d32
Th1nkK1D <witheep@gmail.com>
2021-04-05 15:09:23 +0700

    Update footer link

--- src/sections/footer.svelte
+++ src/sections/footer.svelte
@@ -1,14 +1,35 @@
 <script>
+  import ExternalLink from '../components/external-link.svelte'
   import FollowUs from '../components/follow-us.svelte'
   import Layout from '../components/layout.svelte'
 
-  const logos = [
-    { alt: 'Resolution', src: 'images/logo-resolution.png' },
-    { alt: 'iLaw', src: 'images/logo-ilaw.png' },
-    { alt: 'CONLAB', src: 'images/logo-conlab.png' },
-    { alt: 'คณะก้าวหน้า', src: 'images/logo-klaona.png' },
-    { alt: 'พรรคก้าวไหล', src: 'images/logo-klaokrai.png' },
-    { alt: 'ELECT', src: 'images/logo-elect.png' },
+  const links = [
+    {
+      alt: 'Resolution',
+      src: 'images/logo-resolution.png',
+      href: 'https://www.facebook.com/resolutionconstitution/',
+    },
+    { alt: 'iLaw', src: 'images/logo-ilaw.png', href: 'https://ilaw.or.th/' },
+    {
+      alt: 'CONLAB',
+      src: 'images/logo-conlab.png',
+      href: 'https://www.facebook.com/Constitution9Face/',
+    },
+    {
+      alt: 'คณะก้าวหน้า',
+      src: 'images/logo-klaona.png',
+      href: 'https://progressivemovement.in.th/',
+    },
+    {
+      alt: 'พรรคก้าวไหล',
+      src: 'images/logo-klaokrai.png',
+      href: 'https://www.moveforwardparty.org/',
+    },
+    {
+      alt: 'ELECT',
+      src: 'images/logo-elect.png',
+      href: 'https://elect.in.th/',
+    },
   ]
 </script>
 
@@ -20,8 +41,10 @@
   <div
     class="flex justify-between py-6 md:py-12 w-full max-w-screen-xl mx-auto"
   >
-    {#each logos as { alt, src }}
-      <img {src} {alt} class="my-auto max-w-16 md:max-w-24" />
+    {#each links as { alt, src, href }}
+      <ExternalLink {href} class="my-auto max-w-16 md:max-w-24">
+        <img {src} {alt} />
+      </ExternalLink>
     {/each}
   </div>
 </Layout>
